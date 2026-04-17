b0d204f103ca06af4156db52561bdbd23195624b
rafaellindemann <rafaelld@gmail.com>
2026-04-16 22:14:50 -0300

    eu programo em javascript :D

---
+++ 01-javascript/testes.js
@@ -0,0 +1,23 @@
+// // alert("Olá mundo!")
+// // alert("SEGUNDA MENSAGEM")
+
+// // let nome 
+
+// // nome = "Rafael"
+
+// alert("Nome: " + nome)
+
+// // let idade = 30
+// // alert("Idade: " + idade)
+
+// alert("Nome: " + nome + "\nIdade: " + idade)
+
+// // nome = prompt("Digite seu nome:")
+// // alert("Nome digitado: " + nome)
+
+
+let nota1
+nota1 = prompt("Digite a primeira nota: ")
+nota1 = Number(nota1)
+nota1 = nota1 + 1
+alert(nota1)
