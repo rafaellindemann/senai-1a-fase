dfdd5fffbcac4dcd5d802706a7a8b450e764298f
rafaellindemann <rafaelld@gmail.com>
2026-04-23 20:30:48 -0300

    add exercício futebol

--- 01-javascript/testes.js
+++ 01-javascript/testes.js
@@ -16,8 +16,23 @@
 // // alert("Nome digitado: " + nome)
 
 
-let nota1
-nota1 = prompt("Digite a primeira nota: ")
-nota1 = Number(nota1)
-nota1 = nota1 + 1
-alert(nota1)
+// let nota1
+// nota1 = prompt("Digite a primeira nota: ")
+// nota1 = Number(nota1)
+// nota1 = nota1 + 1
+// alert(nota1)
+
+// ===============================
+// informações
+let vitorias, empates
+let pontos
+// leituras
+vitorias = prompt("Vitórias:")
+vitorias = Number(vitorias)
+
+empates = Number(prompt("Empates: "))
+// processamentos
+pontos = vitorias*3 + empates
+// saídas
+alert("Total de pontos: " + pontos)
+// ===============================
