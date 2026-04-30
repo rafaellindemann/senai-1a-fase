f4786c9692663d4d2f8f13140ef70ef166b06f94
rafaellindemann <rafaelld@gmail.com>
2026-04-30 19:59:49 -0300

    add provisoes cap ganso

--- 03-css/script.js
+++ 03-css/script.js
@@ -1,3 +1,21 @@
+function verificarProvisoes(){
+    // infos
+    let marujos, comida
+    let comidaPorMarujo
+    // entradas
+    marujos = Number(prompt("Quantidade de marujos:"))
+    comida = Number(prompt("Quilos de comida:"))
+    // processamentos
+    comidaPorMarujo = comida / marujos
+    // saidas
+    if(marujos >= 10 && comidaPorMarujo >= 1.5){ // ||
+        document.getElementById('resultado').innerHTML = "Provisões suficientes. Rumo ao horizonte!"
+    }else{
+        document.getElementById('resultado').innerHTML = "Algo está errado. Posseidom não quer ninguém no mar hoje."
+    }
+    
+}
+
 
 function calcularChances(){
     // alert("Aqui vou calcular as chances das criancinhas...")
@@ -17,5 +35,4 @@ function calcularPrecoBrique(){
     alert("Preço para venda: R$" + precoVenda.toFixed(2));
 
     document.getElementById("resultado").innerHTML = "Preço para venda: R$" + precoVenda.toFixed(2)
-
 }
